69f69d89e2d509120fd22f0a2ecdcca2a081297f
vmndz1792 <39140435+vmndz1792@users.noreply.github.com>
2018-06-04 17:55:05 -0400

    Add files via upload

---
+++ onlinelibrary/inc/data.php
@@ -0,0 +1,167 @@
+<?php
+//Books*
+$catalog[101] = [
+    "title" => "The Fault In Our Stars",
+    "img" => "../img/media/thefaultinourstars.jpg",
+   "genre" => "Realistic-Fiction",
+   "format" => "Paperback",
+   "year" => 2012,
+   "category" => "Books",
+   "authors" => [
+       "John Green"
+   ],
+   "publisher" => "Penguin Young Readrs Group",
+   "isbn" => '0525478817'
+];
+$catalog[102] = [
+    "title" => "The Giver",
+    "img" => "../img/media/thegiver.jpg",
+   "genre" => "Science-Fiction",
+   "format" => "Paperback",
+   "year" => 1993,
+   "category" => "Books",
+   "authors" => [
+       "Lois Lowry"
+   ],
+   "publisher" => "Houghton Mifflin Harcourt",
+   "isbn" => '0553571338'
+];
+$catalog[103] = [
+    "title" => "You Are A Badass",
+    "img" => "../img/media/youareabadass.jpg",
+   "genre" => "Self-Help",
+   "format" => "Paperback",
+   "year" => 2013,
+   "category" => "Books",
+   "authors" => [
+       "Jen Sincero"
+   ],
+   "publisher" => "Running Press Book Publishers",
+   "isbn" => '978076244'
+];
+$catalog[104] = [
+    "title" => "Born To Love Cursed To Feel",
+    "img" => "../img/media/borntolove.png",
+   "genre" => "Poetry",
+   "format" => "Paperback",
+   "year" => 2016,
+   "category" => "Books",
+   "authors" => [
+       "Samantha King"
+   ],
+   "publisher" => "Andrews McMeel Publishing",
+   "isbn" => '9781449480950'
+];
+//Movies*
+$catalog[201] = [
+   "title" => "White Chicks",
+   "img" => "../img/media/whitechicks.jpg",
+   "genre" => "Comedy",
+   "format" => "DVD",
+   "year" => 2004,
+   "category" => "Movies",
+   "director" => "Keenen Ivory Wayans",
+   "writers" => [
+       "Marlon Wayans",
+       "Shawn Wayans"
+   ],
+   "stars" => [
+       "Marlon Wayans",
+       "Shawn Wayans",
+       "Busy Philipps"
+   ]
+];
+$catalog[202] = [
+   "title" => "The Notebook",
+   "img" => "../img/media/thenotebook.jpg",
+   "genre" => "Romance",
+   "format" => "DVD",
+   "year" => 2004,
+   "category" => "Movies",
+   "director" => "Nick Cassavetes",
+   "writers" => [
+       "Jeremy Leven",
+		"Jan Sardi"
+   ],
+   "stars" => [
+       "Gena  Rowlands",
+       "James Garner",
+       "Rachel McAdams",
+		"Ryan Gosling"
+				
+   ]
+];
+$catalog[203] = [
+   "title" => "How To Loose A Guy In 10 Days",
+   "img" => "../img/media/howtoloose.jpg",
+   "genre" => "Comedy, Romance",
+   "format" => "DVD",
+   "year" => 2003,
+   "category" => "Movies",
+   "director" => "Donald Petrie",
+   "writers" => [
+       "Michael Alexander",
+       "Jeannie Long"
+   ],
+   "stars" => [
+       "Kate Hudson",
+       "Matthew McConaughey",
+       "Adam Goldberg"
+   ]
+];
+$catalog[204] = [
+   "title" => "The Avengers",
+   "img" => "../img/media/avengers.png",
+   "genre" => "Action, Adventure",
+   "format" => "DVD",
+   "year" => 2012,
+   "category" => "Movies",
+   "director" => "Joss Whedon",
+   "writers" => [
+       "Joss Whedon",
+       "Zak Penn"
+   ],
+   "stars" => [
+       "Chris Evans",
+       "Scarlett Johansson",
+       "Robert Downey Jr."
+   ]
+];
+//Music*
+$catalog[301] = [
+   "title" => "Lemonade",
+   "img" => "img/media/lemonade.jpg",
+   "genre" => "Pop-Music",
+   "format" => "CD",
+   "year" => 2016,
+   "category" => "Music",
+   "artist" => "Beyonce"
+];
+$catalog[302] = [
+   "title" => "Nothing Was The Same",
+   "img" => "../img/media/drake.jpg",
+   "genre" => "Hip-Hop",
+   "format" => "CD",
+   "year" => 2013,
+   "category" => "Music",
+   "artist" => "Drake"
+];
+$catalog[303] = [
+   "title" => "Souled Out",
+   "img" => "../img/media/souledout.jpg",
+   "genre" => "Alternative-R&B",
+   "format" => "CD",
+   "year" => 2014,
+   "category" => "Music",
+   "artist" => "Jhene Aiko"
+];
+$catalog[304] = [
+   "title" => "Anti",
+   "img" => "../img/media/rihanna.png",
+   "genre" => "Hip-Hop",
+   "format" => "CD",
+   "year" => 2016,
+   "category" => "Music",
+   "artist" => "Rihanna"
+];
+?>
---
+++ onlinelibrary/inc/footer.php
@@ -0,0 +1,9 @@
+<div class="footer">
+			<div class="wrapper">
+			<ul>
+				<li><a href="https://www.instagram.com/LaGuardiaCC/">Twitter</a></li>
+				<li><a href="https://www.facebook.com/LaGuardiaCC/">Facebook</a></li>
+			</ul>
+			<p>&copy;<?php echo date("Y") ?> My Online Library Collection</p>
+			</div>
+		</div>
